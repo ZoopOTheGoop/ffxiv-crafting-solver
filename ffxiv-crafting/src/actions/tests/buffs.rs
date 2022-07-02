1374e09cbd93eb6076028c65252881df61ab1a09
ZoopGoop <no@example.com>
2022-07-01 17:48:59 -0700

    Refactor testing functions to be much cleaner

--- ffxiv-crafting/src/actions/tests/buffs.rs
+++ ffxiv-crafting/src/actions/tests/buffs.rs
@@ -6,318 +6,97 @@
 use std::num::NonZeroU8;
 
 use crate::{
-    actions::{buffs::*, Action, ActionOutcome, CpCost},
+    actions::{buffs::*, CpCost},
     buffs::{self, DurationalBuff},
     CraftingState,
 };
 
-use super::CLASSICAL_SIMULATOR;
+use super::{ActionTester, CLASSICAL_SIMULATOR};
 
 #[test]
 fn veneration() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = Veneration;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Veneration made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Veneration in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying veneration has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying veneration does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-
-    let mut expected = state;
-    expected.buffs.progress.veneration = buffs::progress::Veneration::default().activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying veneration did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(Veneration, "Veneration", None)
+        .had_effect()
+        .used_cp(Veneration::CP_COST)
+        .passed_time(true)
+        .triggered_buff(
+            buffs::progress::Veneration::default().activate(0),
+            |buffs| buffs.progress.veneration,
+        );
 }
 
 #[test]
 fn waste_not() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = WasteNot;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Waste Not made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Waste Not in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Waste Not has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Waste Not does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-    assert!(matches!(
-        result.buffs.durability.waste_not,
-        buffs::durability::WasteNot::WasteNot(_)
-    ));
-
-    let mut expected = state;
-    expected.buffs.durability.waste_not = buffs::durability::WasteNot::Inactive.activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Waste Not did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(WasteNot, "Waste Not", None)
+        .had_effect()
+        .used_cp(WasteNot::CP_COST)
+        .passed_time(true)
+        .triggered_buff(
+            // Be explicit on this one to check the right variant
+            buffs::durability::WasteNot::WasteNot(NonZeroU8::new(4).unwrap()),
+            |buffs| buffs.durability.waste_not,
+        );
 }
 
 #[test]
 fn great_strides() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = GreatStrides;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Great Strides made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Great Strides in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Great Strides has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Great Strides does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-
-    let mut expected = state;
-    expected.buffs.quality.great_strides = buffs::quality::GreatStrides::default().activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Great Strides did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(GreatStrides, "Great Strides", None)
+        .had_effect()
+        .used_cp(GreatStrides::CP_COST)
+        .passed_time(true)
+        .triggered_buff(
+            buffs::quality::GreatStrides::default().activate(0),
+            |buffs| buffs.quality.great_strides,
+        );
 }
 
 #[test]
 fn innovation() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = Innovation;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Innovation made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Innovation in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Innovation has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Innovation does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-
-    let mut expected = state;
-    expected.buffs.quality.innovation = buffs::quality::Innovation::default().activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Innovation did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(Innovation, "Innovation", None)
+        .had_effect()
+        .used_cp(Innovation::CP_COST)
+        .passed_time(true)
+        .triggered_buff(buffs::quality::Innovation::default().activate(0), |buffs| {
+            buffs.quality.innovation
+        });
 }
 
 #[test]
 fn final_appraisal() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = FinalAppraisal;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Final Appraisal made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Final Appraisal in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Final Appraisal has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Final Appraisal does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-    assert!(
-        !delta.time_passed,
-        "Time passed after using final appraisal"
-    );
-
-    let mut expected = state;
-    expected.buffs.progress.final_appraisal =
-        buffs::progress::FinalAppraisal::default().activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Final Appraisal did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(FinalAppraisal, "Final Appraisal", None)
+        .had_effect()
+        .used_cp(FinalAppraisal::CP_COST)
+        .passed_time(false)
+        .triggered_buff(
+            buffs::progress::FinalAppraisal::default().activate(0),
+            |buffs| buffs.progress.final_appraisal,
+        );
 }
 
 #[test]
 fn waste_not_2() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = WasteNot2;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Waste Not 2 made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Waste Not 2 in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Waste Not 2 has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Waste Not 2 does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-    assert_eq!(
-        result.buffs.durability.waste_not,
-        buffs::durability::WasteNot::WasteNot2(NonZeroU8::new(8).unwrap()),
-    );
-
-    let mut expected = state;
-    expected.buffs.durability.waste_not = buffs::durability::WasteNot::Inactive.activate(4);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Waste Not 2 did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(WasteNot2, "Waste Not 2", None)
+        .had_effect()
+        .used_cp(WasteNot2::CP_COST)
+        .passed_time(true)
+        .triggered_buff(
+            // Be explicit on this one to check the right variant
+            buffs::durability::WasteNot::WasteNot2(NonZeroU8::new(8).unwrap()),
+            |buffs| buffs.durability.waste_not,
+        );
 }
 
 #[test]
 fn manipulation() {
-    let state = CraftingState::new_simulation(&CLASSICAL_SIMULATOR);
-
-    let action = Manipulation;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("Manipulation made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute Manipulation in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying Manipulation has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert!(
-        result.curr_cp < state.curr_cp,
-        "Applying Manipulation does not cost CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-
-    let mut expected = state;
-    expected.buffs.durability.manipulation = buffs::durability::Manipulation::default().activate(0);
-    expected.curr_cp -= action.cp_cost(&state);
-
-    assert_eq!(
-        result, expected,
-        "Applying Manipulation did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(Manipulation, "Manipulation", None)
+        .had_effect()
+        .used_cp(Manipulation::CP_COST)
+        .passed_time(true)
+        .triggered_buff(
+            // Be explicit on this one to check the right variant
+            buffs::durability::Manipulation::default().activate(0),
+            |buffs| buffs.durability.manipulation,
+        );
 }
 
 #[test]
@@ -326,40 +105,12 @@ fn heart_and_soul() {
     state.buffs.specialist_actions = buffs::misc::SpecialistActions::Availalble(3);
     let state = state;
 
-    let action = HeartAndSoul;
-    let outcome = action.prospective_act(&state);
-    let delta = match outcome {
-        Ok(outcome) => match outcome {
-            ActionOutcome::InProgress(delta) => delta,
-            other => panic!("HeartAndSoul made craft fail or succeed: {:?}", other),
-        },
-        Err(err) => panic!(
-            "Error trying to execute HeartAndSoul in a valid state: {}",
-            err
-        ),
-    };
-
-    let result = state + delta;
-
-    assert_ne!(
-        state, result,
-        "Applying HeartAndSoul has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state, result, delta
-    );
-    assert_eq!(
-        result.curr_cp, state.curr_cp,
-        "Applying HeartAndSoul costs CP (should only cost specialist actions);\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
-        state,
-        result,
-        delta
-    );
-
-    let mut expected = state;
-    expected.buffs.heart_and_soul = buffs::misc::HeartAndSoul::Active;
-    expected.buffs.specialist_actions = buffs::misc::SpecialistActions::Availalble(2);
-
-    assert_eq!(
-        result, expected,
-        "Applying HeartAndSoul did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", expected, result, delta, 
-    )
+    ActionTester::make(HeartAndSoul, "Heart And Soul", Some(state))
+        .had_effect()
+        .used_cp(0)
+        .used_delineation()
+        .passed_time(false)
+        .triggered_buff(buffs::misc::HeartAndSoul::Active, |buffs| {
+            buffs.heart_and_soul
+        });
 }
--- ffxiv-crafting/src/actions/tests/mod.rs
+++ ffxiv-crafting/src/actions/tests/mod.rs
@@ -2,15 +2,20 @@
 
 mod buffs;
 
+use std::fmt::Debug;
+
 use lazy_static::lazy_static;
 use rand::RngCore;
 
 use crate::{
+    actions::Action,
     conditions::QARegularConditions,
     quality_map::HQMap,
     recipe::{RLvl, Recipe},
-    CharacterStats, CraftingSimulator,
+    CharacterStats, CraftingSimulator, CraftingState, buffs::{BuffState, Buff},
 };
+
+use super::{ActionOutcome, StateDelta};
 
 /// My character's stats with melded Pactmaker under Cunning Craftsman's Syrup and Tsai you Vounou in 6.1
 const TEST_STATS: CharacterStats = CharacterStats {
@@ -63,4 +68,131 @@ fn verify_recipe_modifiers_classical() {
         "Classical gear should have 70 durability, got {}",
         TEST_RECIPE_CLASSICAL.max_quality
     );
+}
+
+struct ActionTester<'a, A: Action + Copy> {
+    #[allow(dead_code)]
+    action: A,
+    name: &'a str,
+    state: CraftingState<'static, QARegularConditions, HQMap>,
+    delta: StateDelta,
+}
+
+impl<'a, A: Action + Copy> ActionTester<'a, A> {
+    fn make(
+        action: A,
+        name: &'a str,
+        state: Option<CraftingState<'static, QARegularConditions, HQMap>>,
+    ) -> Self {
+        let state = state.unwrap_or_else(|| CraftingState::new_simulation(&CLASSICAL_SIMULATOR));
+
+        let outcome = action.prospective_act(&state);
+        let delta = match outcome {
+            Ok(outcome) => match outcome {
+                ActionOutcome::InProgress(delta) => delta,
+                other => panic!("{} made craft fail or succeed: {:?}", name, other),
+            },
+            Err(err) => panic!("Error trying to execute {} in a valid state: {}", name, err),
+        };
+
+        Self {
+            action,
+            name,
+            state,
+            delta,
+        }
+    }
+
+    fn used_cp(self, amount: i16) -> Self {
+        let result = self.state + self.delta;
+
+        assert_eq!(
+            result.curr_cp, self.state.curr_cp - amount,
+            "Applying {} does not cost/give {} CP;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
+            self.name,
+            amount,
+            self.state,
+            result,
+            self.delta
+        );
+
+        self
+    }
+
+    fn used_delineation(self) -> Self {
+        use crate::buffs::misc::SpecialistActions;
+
+        let result = self.state + self.delta;
+
+        let expected = match self.state.buffs.specialist_actions {
+            SpecialistActions::Availalble(n @ 2..=u8::MAX) => SpecialistActions::Availalble(n-1),
+            SpecialistActions::Availalble(1) => SpecialistActions::Unavailable,
+            SpecialistActions::Availalble(0) => unreachable!(),
+            SpecialistActions::NotSpecialist => panic!("Action {} should not execute - character is not specialist", self.name),
+            SpecialistActions::Unavailable => panic!("Action {} should not execute - out of specialist actions", self.name),
+        };
+
+        assert_eq!(
+            result.buffs.specialist_actions, expected,
+            "Applying {} does not cost a specialist action;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
+            self.name,
+            self.state,
+            result,
+            self.delta
+        );
+
+        self
+    }
+
+    #[allow(dead_code)]
+    fn expected_changes<F>(self, alteration: F) -> Self
+    where
+        F: Fn(
+            CraftingState<'static, QARegularConditions, HQMap>,
+        ) -> CraftingState<'static, QARegularConditions, HQMap>,
+    {
+        let expected = alteration(self.state);
+        let result = self.state + self.delta;
+
+        assert_eq!(
+            result, expected,
+            "Applying {} did not change the correct things\n\texpected: {:?}\n\tresult: {:?}\n\tdelta: {:?}", 
+            self.name, expected, result, self.delta, 
+        );
+
+        self
+    }
+
+    fn had_effect(self) -> Self {
+        let result = self.state + self.delta;
+        assert_ne!(
+            self.state, result,
+            "Applying {} has no effect;\n\tstate: {:?}\n\tresult: {:?}\n\tdelta: {:?}",
+            self.name, self.state, result, self.delta
+        );
+
+        self
+    }
+
+    fn passed_time(self, should_pass_time: bool) -> Self {
+        if self.delta.time_passed && !should_pass_time {
+            panic!("Applying {} causes time to pass when it shouldn't;\n\tstate:{:?}, delta: {:?}", self.name, self.state, self.delta);
+        } else if !self.delta.time_passed && should_pass_time {
+            panic!("Applying {} doesn't cause time to pass when it should;\n\tstate:{:?}, delta: {:?}", self.name, self.state, self.delta);
+        }
+
+        self
+    }
+
+    fn triggered_buff<B, F1>(self, buff: B, get_buff: F1) -> Self 
+    where
+        B: Buff+Eq+Debug,
+        F1: Fn(BuffState) -> B,
+    {
+        let result = self.state + self.delta;
+        assert_eq!(get_buff(result.buffs), buff, "Applying {} does not cause buff {:?} to be applied;\n\tstate:{:?}, result: {:?}, delta: {:?}", 
+        self.name, buff, self.state, result, self.delta);
+
+        self
+    }
 }
